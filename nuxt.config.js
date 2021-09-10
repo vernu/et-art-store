fcecc41569308b1d25886a894964dc6c7ce9890a
isra el <vernu1997@gmail.com>
2021-09-10 16:26:41 +0300

    disabled ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,12 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+  
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - et-art-store-frontend',
