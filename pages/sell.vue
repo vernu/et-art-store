3624a96416766e3a889aaa8722e955929d0f003e
isra el <vernu1997@gmail.com>
2021-09-24 19:29:34 +0300

    worked on item image upload

--- pages/sell.vue
+++ pages/sell.vue
@@ -1,20 +1,30 @@
 <template>
   <v-container>
-    <h2>Sell</h2>
+    <v-row justify="center">
+      <v-col sm="12" md="4">
+        <h2>Sell</h2>
 
-    <v-form>
-      <v-text-field label="Item Name" v-model="itemName"></v-text-field>
-      <v-text-field
-        label="Price"
-        type="number"
-        v-model="price"
-        required
-      ></v-text-field>
+        <v-form>
+          <v-text-field label="Item Name" v-model="itemName"></v-text-field>
+          <v-text-field
+            label="Price"
+            type="number"
+            v-model="price"
+            required
+          ></v-text-field>
 
-      <v-text-field label="Image Url" v-model="imageUrl"></v-text-field>
-      <v-progress-circular indeterminate v-if="isSubmitting" />
-      <v-btn v-else v-on:click="submit">Submit</v-btn>
-    </v-form>
+          <v-file-input
+            v-model="itemImage"
+            accept="image/*"
+            prepend-icon="mdi-camera"
+            label="Image"
+          ></v-file-input>
+
+          <v-progress-circular indeterminate v-if="isSubmitting" />
+          <v-btn v-else v-on:click="submit">Submit</v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -25,14 +35,22 @@ export default {
       isSubmitting: false,
       itemName: "",
       price: null,
+      itemImage: null,
       imageUrl: null,
     };
   },
   methods: {
     async submit() {
+      const timestamp = new Date().getTime();
       this.isSubmitting = true;
-      const ref = this.$fire.firestore.collection("items").doc();
+      const imageRef = this.$fire.storage
+        .ref()
+        .child(`items/${timestamp}-${this.itemImage.name}`);
       try {
+        const snapshot = await imageRef.put(this.itemImage);
+        const imageDownloadUrl = await snapshot.ref.getDownloadURL();
+        this.imageUrl = imageDownloadUrl;
+        const ref = this.$fire.firestore.collection("items").doc();
         await ref.set({
           name: this.itemName,
           price: parseInt(this.price),
@@ -42,6 +60,7 @@ export default {
         console.log(e);
       } finally {
         this.isSubmitting = false;
+        this.$router.push("/");
       }
     },
   },
